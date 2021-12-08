8e3f1a440195b63fd4700134547e138f6fc7fe45
MerryNecro <pankiahoy@gmail.com>
2021-12-08 21:55:05 +0400

    add build

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 MD C:\Results\bin\Jenkins
 MD C:\Results\bin\JenkinsTest
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
-dotnet restore C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob
-msbuild.exe C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob\Jenkins /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
-msbuild.exe C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsMsTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
+dotnet restore C:\Users\Ubunta\AppData\Local\Jenkins\.jenkins\workspace\MyJob
+msbuild.exe C:\Users\Ubunta\AppData\Local\Jenkins\.jenkins\workspace\MyJob\Jenkins /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
+msbuild.exe C:\Users\Ubunta\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsMsTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
